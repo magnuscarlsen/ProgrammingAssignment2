4ac37b9f7e430fe6a307acb1d1ffd8efe85b98b7
magnuscarlsen <carlsen.magnus@gmail.com>
2017-05-11 11:36:59 +0200

    R programming assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,52 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The functions calculates/pulls the inverse of a matrix stored in a different
+## environment, depending if it is already done.
 
-## Write a short comment describing this function
+
+
+## makes a list object whihc has a bunch of functions whihc sets ...
+## variables in an environment. They be altered with the functions created
 
 makeCacheMatrix <- function(x = matrix()) {
+    m <- NULL
+    set <- function(y) {
+        x <<- y
+        m <<- NULL
+    }
+    
+    get <- function() x
+    
+    setinv <- function(solve) m <<- solve
+    getinv <- function() m
+    
+    list(set = set, get = get,
+         setinv = setinv,
+         getinv = getinv)
 
 }
 
-
-## Write a short comment describing this function
+## this function looks for an inverse, if it exists already 
+## in the cache it pulls from there otherwise calcs new and 
+## sets to the object.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    m <- x$getinv()
+    if(!is.null(m)) {
+        message("getting cached data")
+        return(m)
+    }
+    data <- x$get()
+    m <- solve(data)
+    x$setinv(m)
+    m
+           ## Return a matrix that is the inverse of 'x'
 }
+
+## to test that the code works you have to source this and then add a few 
+## following lines in the console:
+
+## mattes<-matrix(c(1:4,1:4,1),3,3)
+## matr<-makeCacheMatrix(mattes)
+## cacheSolve(matr)
+## cacheSolve(matr) 
+
+## the output shows that it reads from the cache.
